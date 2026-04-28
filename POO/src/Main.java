e310bbfc7abc3d942998b73c57e86918848d9343
caalarcon <camily.alarcon24@gmail.com>
2026-04-28 08:14:23 -0300

    Add files via upload

---
+++ POO/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int resultado = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: divisão por zero!");
+        }
+    }
+}
