0f32765307ec107b34f0b1506626e9113d2f6458
caalarcon <camily.alarcon24@gmail.com>
2026-08-21 08:40:48 -0300

    Delete Empresa directory

--- Empresa/src/main/java/com/example/empresa/Repositories/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.empresa.Repositories;
-
-import com.example.empresa.entities.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Repository responsável por realizar as operações de acesso ao banco de dados.
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
